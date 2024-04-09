cf8ea876ef31565cf04f2ef3397252477848ef4f
rafatu07 <rafa.tu@hotmail.com>
2024-04-08 23:54:51 -0300

    backup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,51 +1,16 @@
-const { WebSocketServer } = require("ws");
-const dotenv = require("dotenv");
+const { WebSocketServer } = require("ws")
+const dotenv = require("dotenv")
 
-dotenv.config();
+dotenv.config()
 
-const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
-
-// Array para armazenar os usuários ativos
-let activeUsers = [];
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
 
 wss.on("connection", (ws) => {
-    ws.on("error", console.error);
-
-    // Envia a lista de usuários ativos para o novo cliente conectado
-    ws.send(JSON.stringify({ type: "activeUsersUpdate", users: activeUsers }));
+    ws.on("error", console.error)
 
     ws.on("message", (data) => {
-        const message = JSON.parse(data);
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
 
-        if (message.type === "join") {
-            handleUserJoin(message.userName);
-        } else if (message.type === "leave") {
-            handleUserLeave(message.userName);
-        } else {
-            // Reenvia a mensagem para todos os clientes
-            wss.clients.forEach((client) => client.send(data));
-        }
-    });
-
-    console.log("client connected");
-});
-
-// Função para lidar com a entrada de um novo usuário
-function handleUserJoin(userName) {
-    if (!activeUsers.includes(userName)) {
-        activeUsers.push(userName);
-        broadcastActiveUsersUpdate();
-    }
-}
-
-// Função para lidar com a saída de um usuário
-function handleUserLeave(userName) {
-    activeUsers = activeUsers.filter(user => user !== userName);
-    broadcastActiveUsersUpdate();
-}
-
-// Função para enviar uma mensagem de atualização da lista de usuários ativos para todos os clientes
-function broadcastActiveUsersUpdate() {
-    const message = JSON.stringify({ type: "activeUsersUpdate", users: activeUsers });
-    wss.clients.forEach((client) => client.send(message));
-}
+    console.log("client connected")
+})
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,14 +1,13 @@
 // login elements
-const login = document.querySelector(".login");
-const loginForm = login.querySelector(".login__form");
-const loginInput = login.querySelector(".login__input");
+const login = document.querySelector(".login")
+const loginForm = login.querySelector(".login__form")
+const loginInput = login.querySelector(".login__input")
 
 // chat elements
-const chat = document.querySelector(".chat");
-const chatForm = chat.querySelector(".chat__form");
-const chatInput = chat.querySelector(".chat__input");
-const chatMessages = chat.querySelector(".chat__messages");
-const activeUsersList = document.querySelector(".active-users"); // Seleciona a lista de usuários ativos
+const chat = document.querySelector(".chat")
+const chatForm = chat.querySelector(".chat__form")
+const chatInput = chat.querySelector(".chat__input")
+const chatMessages = chat.querySelector(".chat__messages")
 
 const colors = [
     "cadetblue",
@@ -17,107 +16,91 @@ const colors = [
     "darkkhaki",
     "hotpink",
     "gold"
-];
+]
 
-const user = { id: "", name: "", color: "" };
+const user = { id: "", name: "", color: "" }
 
-let websocket;
+let websocket
 
-// Função para criar elementos de mensagem do próprio usuário
 const createMessageSelfElement = (content) => {
-    const div = document.createElement("div");
-    div.classList.add("message--self");
-    div.innerHTML = content;
-    return div;
-};
+    const div = document.createElement("div")
 
-// Função para criar elementos de mensagem de outros usuários
+    div.classList.add("message--self")
+    div.innerHTML = content
+
+    return div
+}
+
 const createMessageOtherElement = (content, sender, senderColor) => {
-    const div = document.createElement("div");
-    const span = document.createElement("span");
+    const div = document.createElement("div")
+    const span = document.createElement("span")
 
-    div.classList.add("message--other");
+    div.classList.add("message--other")
 
-    span.classList.add("message--sender");
-    span.style.color = senderColor;
+    span.classList.add("message--sender")
+    span.style.color = senderColor
 
-    div.appendChild(span);
-    span.innerHTML = sender;
-    div.innerHTML += content;
+    div.appendChild(span)
 
-    return div;
-};
+    span.innerHTML = sender
+    div.innerHTML += content
 
-// Função para gerar uma cor aleatória
+    return div
+}
+
 const getRandomColor = () => {
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    return colors[randomIndex];
-};
+    const randomIndex = Math.floor(Math.random() * colors.length)
+    return colors[randomIndex]
+}
 
-// Função para rolar a tela para baixo
 const scrollScreen = () => {
     window.scrollTo({
         top: document.body.scrollHeight,
         behavior: "smooth"
-    });
-};
+    })
+}
 
-// Função para processar mensagens recebidas do servidor
 const processMessage = ({ data }) => {
-    const { type, userId, userName, userColor, content, users } = JSON.parse(data);
+    const { userId, userName, userColor, content } = JSON.parse(data)
 
-    // Verifica o tipo de mensagem recebida
-    if (type === "activeUsersUpdate") {
-        updateActiveUsers(users); // Chama a função para atualizar a lista de usuários ativos
-    } else {
-        const message = userId == user.id ?
-            createMessageSelfElement(content) :
-            createMessageOtherElement(content, userName, userColor);
-        chatMessages.appendChild(message);
-        scrollScreen();
-    }
-};
+    const message =
+        userId == user.id
+            ? createMessageSelfElement(content)
+            : createMessageOtherElement(content, userName, userColor)
 
-// Função para lidar com o envio do formulário de login
+    chatMessages.appendChild(message)
+
+    scrollScreen()
+}
+
 const handleLogin = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    user.id = crypto.randomUUID();
-    user.name = loginInput.value;
-    user.color = getRandomColor();
+    user.id = crypto.randomUUID()
+    user.name = loginInput.value
+    user.color = getRandomColor()
 
-    login.style.display = "none";
-    chat.style.display = "flex";
+    login.style.display = "none"
+    chat.style.display = "flex"
 
-    websocket = new WebSocket("wss://web-chat-back-ende.onrender.com");
-    websocket.onmessage = processMessage;
-};
+    websocket = new WebSocket("wss://web-chat-back-ende.onrender.com")
+    websocket.onmessage = processMessage
+}
 
-// Função para enviar uma mensagem
 const sendMessage = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     const message = {
         userId: user.id,
         userName: user.name,
         userColor: user.color,
         content: chatInput.value
-    };
+    }
 
-    websocket.send(JSON.stringify(message));
-    chatInput.value = "";
-};
+    websocket.send(JSON.stringify(message))
 
-// Função para atualizar a lista de usuários ativos
-function updateActiveUsers(users) {
-    activeUsersList.innerHTML = ""; // Limpa a lista antes de adicionar os novos usuários
-    users.forEach(user => {
-        const userElement = document.createElement("li");
-        userElement.textContent = user;
-        activeUsersList.appendChild(userElement);
-    });
+    chatInput.value = ""
 }
 
-// Adiciona os event listeners aos formulários
-loginForm.addEventListener("submit", handleLogin);
-chatForm.addEventListener("submit", sendMessage);
+loginForm.addEventListener("submit", handleLogin)
+chatForm.addEventListener("submit", sendMessage)
