34522dbc82adae78b3ce35ef9d294358d043dfe0
rafatu07 <rafa.tu@hotmail.com>
2024-04-08 23:25:22 -0300

    2325

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,22 +5,30 @@ dotenv.config();
 
 const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
 
+const activeUsers = new Set(); // Conjunto para armazenar os nomes dos usuários ativos
+
 wss.on("connection", (ws) => {
     ws.on("error", console.error);
+
+    console.log("client connected");
 
     ws.on("message", (data) => {
         const message = JSON.parse(data);
 
         if (message.type === "join") {
+            activeUsers.add(message.userName); // Adiciona o usuário ativo ao conjunto
             const userJoinedMessage = {
                 type: "userJoined",
-                userName: message.userName
+                userName: message.userName,
+                users: Array.from(activeUsers) // Envia a lista atualizada de usuários ativos para todos os clientes
             };
             broadcast(JSON.stringify(userJoinedMessage));
         } else if (message.type === "leave") {
+            activeUsers.delete(message.userName); // Remove o usuário ativo do conjunto
             const userLeftMessage = {
                 type: "userLeft",
-                userName: message.userName
+                userName: message.userName,
+                users: Array.from(activeUsers) // Envia a lista atualizada de usuários ativos para todos os clientes
             };
             broadcast(JSON.stringify(userLeftMessage));
         } else {
@@ -28,8 +36,6 @@ wss.on("connection", (ws) => {
             wss.clients.forEach((client) => client.send(data));
         }
     });
-
-    console.log("client connected");
 });
 
 // Função para enviar uma mensagem para todos os clientes conectados
