3391dfd7d29a3d2c6485f020ae150d9db9c9e5b5
rafatu07 <rafa.tu@hotmail.com>
2024-04-08 22:57:34 -0300

    atualizaçao 2257

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,56 +1,39 @@
 const { WebSocketServer } = require("ws");
-const fs = require("fs");
-const path = require("path");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-const MESSAGES_FILE_PATH = "./messages/messages.json";
-
-// Função para salvar a mensagem no arquivo JSON
-function saveMessage(message) {
-    let messages = [];
-    if (fs.existsSync(MESSAGES_FILE_PATH)) {
-        messages = JSON.parse(fs.readFileSync(MESSAGES_FILE_PATH, "utf-8"));
-    }
-    messages.push(message);
-    fs.writeFileSync(MESSAGES_FILE_PATH, JSON.stringify(messages, null, 2));
-}
-
-// Função para criar o arquivo messages.json se ele não existir
-function createMessagesFile() {
-    // Verifica se o arquivo já existe
-    if (!fs.existsSync(MESSAGES_FILE_PATH)) {
-        // Cria o diretório se não existir
-        const directory = path.dirname(MESSAGES_FILE_PATH);
-        fs.mkdirSync(directory, { recursive: true });
-
-        // Cria o arquivo messages.json
-        fs.writeFileSync(MESSAGES_FILE_PATH, "[]");
-
-        console.log("Arquivo messages.json criado com sucesso.");
-    } else {
-        console.log("O arquivo messages.json já existe.");
-    }
-}
-
-// Chama a função para criar o arquivo messages.json
-createMessagesFile();
-
-// Inicia o servidor WebSocket
 const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
 
-// Evento de conexão com WebSocket
 wss.on("connection", (ws) => {
     ws.on("error", console.error);
 
-    // Evento de recebimento de mensagem
+    // Quando um usuário entra no chat
+    ws.on("join", (userName) => {
+        const message = {
+            type: "userJoined",
+            userName: userName
+        };
+        broadcast(JSON.stringify(message));
+    });
+
+    // Quando um usuário sai do chat
+    ws.on("leave", (userName) => {
+        const message = {
+            type: "userLeft",
+            userName: userName
+        };
+        broadcast(JSON.stringify(message));
+    });
+
     ws.on("message", (data) => {
-        const message = JSON.parse(data);
-        saveMessage(message);
-        // Envia a mensagem para todos os clientes conectados
         wss.clients.forEach((client) => client.send(data.toString()));
     });
 
-    console.log("Cliente conectado.");
+    console.log("client connected");
 });
+
+// Função para enviar uma mensagem para todos os clientes conectados
+function broadcast(message) {
+    wss.clients.forEach((client) => client.send(message));
+}
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,14 +1,12 @@
-// login elements
-const login = document.querySelector(".login")
-const loginForm = login.querySelector(".login__form")
-const loginInput = login.querySelector(".login__input")
+const login = document.querySelector(".login");
+const loginForm = login.querySelector(".login__form");
+const loginInput = login.querySelector(".login__input");
 
-// chat elements
-const chat = document.querySelector(".chat")
-const chatForm = chat.querySelector(".chat__form")
-const chatInput = chat.querySelector(".chat__input")
-const chatMessages = chat.querySelector(".chat__messages")
-const clearMessagesButton = document.getElementById("clearMessagesButton"); // Adicione o elemento do botão de limpar mensagens
+const chat = document.querySelector(".chat");
+const chatForm = chat.querySelector(".chat__form");
+const chatInput = chat.querySelector(".chat__input");
+const chatMessages = chat.querySelector(".chat__messages");
+const activeUsersList = document.querySelector(".active-users");
 
 const colors = [
     "cadetblue",
@@ -17,115 +15,106 @@ const colors = [
     "darkkhaki",
     "hotpink",
     "gold"
-]
+];
 
-const user = { id: "", name: "", color: "" }
+const user = { id: "", name: "", color: "" };
 
-let websocket
+let websocket;
 
 const createMessageSelfElement = (content) => {
-    const div = document.createElement("div")
-
-    div.classList.add("message--self")
-    div.innerHTML = content
-
-    return div
-}
+    const div = document.createElement("div");
+    div.classList.add("message--self");
+    div.innerHTML = content;
+    return div;
+};
 
 const createMessageOtherElement = (content, sender, senderColor) => {
-    const div = document.createElement("div")
-    const span = document.createElement("span")
+    const div = document.createElement("div");
+    const span = document.createElement("span");
 
-    div.classList.add("message--other")
+    div.classList.add("message--other");
 
-    span.classList.add("message--sender")
-    span.style.color = senderColor
+    span.classList.add("message--sender");
+    span.style.color = senderColor;
 
-    div.appendChild(span)
+    div.appendChild(span);
+    span.innerHTML = sender;
+    div.innerHTML += content;
 
-    span.innerHTML = sender
-    div.innerHTML += content
-
-    return div
-}
+    return div;
+};
 
 const getRandomColor = () => {
-    const randomIndex = Math.floor(Math.random() * colors.length)
-    return colors[randomIndex]
-}
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+};
 
 const scrollScreen = () => {
     window.scrollTo({
         top: document.body.scrollHeight,
         behavior: "smooth"
-    })
-}
+    });
+};
 
 const processMessage = ({ data }) => {
-    const { userId, userName, userColor, content } = JSON.parse(data)
+    const { type, userName } = JSON.parse(data);
 
-    const message =
-        userId == user.id
-            ? createMessageSelfElement(content)
-            : createMessageOtherElement(content, userName, userColor)
-
-    chatMessages.appendChild(message)
-
-    scrollScreen()
-
-    // Exiba o botão de limpar mensagens se o usuário for o administrador
-    if (user.name === "admin") {
-        clearMessagesButton.style.display = "block";
+    if (type === "userJoined") {
+        const userElement = document.createElement("li");
+        userElement.textContent = userName;
+        activeUsersList.appendChild(userElement); // Adiciona o usuário à lista de usuários online
+    } else if (type === "userLeft") {
+        const userList = activeUsersList.querySelectorAll("li");
+        userList.forEach(user => {
+            if (user.textContent === userName) {
+                user.remove(); // Remove o usuário da lista de usuários online
+            }
+        });
     } else {
-        clearMessagesButton.style.display = "none";
+        // Se a mensagem for uma mensagem de chat normal, processe-a normalmente
+        const { userId, userName, userColor, content } = JSON.parse(data);
+        const message = userId == user.id ?
+            createMessageSelfElement(content) :
+            createMessageOtherElement(content, userName, userColor);
+        chatMessages.appendChild(message);
+        scrollScreen();
     }
-}
+};
 
 const handleLogin = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    user.id = crypto.randomUUID()
-    user.name = loginInput.value
-    user.color = getRandomColor()
+    user.id = crypto.randomUUID();
+    user.name = loginInput.value;
+    user.color = getRandomColor();
 
-    login.style.display = "none"
-    chat.style.display = "flex"
+    login.style.display = "none";
+    chat.style.display = "flex";
 
-    // Verifique se o usuário é admin e mostre o botão de limpar mensagens se for o caso
-    if (user.name === "admin") {
-        clearMessagesButton.style.display = "block";
-    } else {
-        clearMessagesButton.style.display = "none";
-    }
-
-    // WebSocket: conecte-se ao servidor WebSocket
     websocket = new WebSocket("wss://web-chat-back-ende.onrender.com");
     websocket.onmessage = processMessage;
-}
+
+    // Envie uma mensagem ao servidor informando que o usuário entrou no chat
+    const joinMessage = {
+        type: "join",
+        userName: user.name
+    };
+    websocket.send(JSON.stringify(joinMessage));
+};
 
 const sendMessage = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     const message = {
         userId: user.id,
         userName: user.name,
         userColor: user.color,
         content: chatInput.value
-    }
+    };
 
-    websocket.send(JSON.stringify(message))
+    websocket.send(JSON.stringify(message));
+    chatInput.value = "";
+};
 
-    chatInput.value = ""
-}
-
-// Adicione um evento de clique para o botão de limpar mensagens
-clearMessagesButton.addEventListener("click", () => {
-    // Limpe as mensagens da interface do usuário
-    chatMessages.innerHTML = "";
-
-    // Oculte o botão de limpar mensagens
-    clearMessagesButton.style.display = "none";
-});
-
-loginForm.addEventListener("submit", handleLogin)
-chatForm.addEventListener("submit", sendMessage)
+loginForm.addEventListener("submit", handleLogin);
+chatForm.addEventListener("submit", sendMessage);
