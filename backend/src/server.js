0db6022124c75338f1e4438ea5136c2ba9e2bb69
rafatu07 <rafa.tu@hotmail.com>
2024-04-08 21:49:02 -0300

    backup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,56 +1,16 @@
-const { WebSocketServer } = require("ws");
-const fs = require("fs");
-const path = require("path");
-const dotenv = require("dotenv");
+const { WebSocketServer } = require("ws")
+const dotenv = require("dotenv")
 
-dotenv.config();
+dotenv.config()
 
-const MESSAGES_FILE_PATH = "../messages//messages.json";
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
 
-// Função para salvar a mensagem no arquivo JSON
-function saveMessage(message) {
-    let messages = [];
-    if (fs.existsSync(MESSAGES_FILE_PATH)) {
-        messages = JSON.parse(fs.readFileSync(MESSAGES_FILE_PATH, "utf-8"));
-    }
-    messages.push(message);
-    fs.writeFileSync(MESSAGES_FILE_PATH, JSON.stringify(messages, null, 2));
-}
-
-// Função para criar o arquivo messages.json se ele não existir
-function createMessagesFile() {
-    // Verifica se o arquivo já existe
-    if (!fs.existsSync(MESSAGES_FILE_PATH)) {
-        // Cria o diretório se não existir
-        const directory = path.dirname(MESSAGES_FILE_PATH);
-        fs.mkdirSync(directory, { recursive: true });
-
-        // Cria o arquivo messages.json
-        fs.writeFileSync(MESSAGES_FILE_PATH, "[]");
-
-        console.log("Arquivo messages.json criado com sucesso.");
-    } else {
-        console.log("O arquivo messages.json já existe.");
-    }
-}
-
-// Chama a função para criar o arquivo messages.json
-createMessagesFile();
-
-// Inicia o servidor WebSocket
-const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
-
-// Evento de conexão com WebSocket
 wss.on("connection", (ws) => {
-    ws.on("error", console.error);
+    ws.on("error", console.error)
 
-    // Evento de recebimento de mensagem
     ws.on("message", (data) => {
-        const message = JSON.parse(data);
-        saveMessage(message);
-        // Envia a mensagem para todos os clientes conectados
-        wss.clients.forEach((client) => client.send(data.toString()));
-    });
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
 
-    console.log("Cliente conectado.");
-});
+    console.log("client connected")
+})
