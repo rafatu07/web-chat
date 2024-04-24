068b67551ab152ac49c9b890eee54966c0b8b41a
rafatu07 <rafa.tu@hotmail.com>
2024-04-23 22:07:16 -0300

    voltar 2207

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,43 +1,16 @@
-const { WebSocketServer } = require("ws");
-const dotenv = require("dotenv");
+const { WebSocketServer } = require("ws")
+const dotenv = require("dotenv")
 
-dotenv.config();
+dotenv.config()
 
-const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
-
-function heartbeat() {
-    this.isAlive = true;
-}
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
 
 wss.on("connection", (ws) => {
-    ws.isAlive = true;
-
-    ws.on("pong", heartbeat);
-
-    ws.on("error", console.error);
+    ws.on("error", console.error)
 
     ws.on("message", (data) => {
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data.toString());
-            }
-        });
-    });
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
 
-    console.log("client connected");
-});
-
-const interval = setInterval(() => {
-    wss.clients.forEach((ws) => {
-        if (ws.isAlive === false) {
-            return ws.terminate();
-        }
-
-        ws.isAlive = false;
-        ws.ping(() => {});
-    });
-}, 30000); // Envia pings a cada 30 segundos
-
-wss.on("close", () => {
-    clearInterval(interval);
-});
+    console.log("client connected")
+})
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,8 +1,13 @@
-const loginForm = document.querySelector(".login__form");
-const loginInput = document.querySelector(".login__input");
-const chatForm = document.querySelector(".chat__form");
-const chatInput = document.querySelector(".chat__input");
-const chatMessages = document.querySelector(".chat__messages");
+// login elements
+const login = document.querySelector(".login")
+const loginForm = login.querySelector(".login__form")
+const loginInput = login.querySelector(".login__input")
+
+// chat elements
+const chat = document.querySelector(".chat")
+const chatForm = chat.querySelector(".chat__form")
+const chatInput = chat.querySelector(".chat__input")
+const chatMessages = chat.querySelector(".chat__messages")
 
 const colors = [
     "cadetblue",
@@ -11,98 +16,91 @@ const colors = [
     "darkkhaki",
     "hotpink",
     "gold"
-];
+]
 
-const user = { id: "", name: "", color: "" };
-let websocket;
+const user = { id: "", name: "", color: "" }
+
+let websocket
 
 const createMessageSelfElement = (content) => {
-    const div = document.createElement("div");
-    div.classList.add("message--self");
-    div.innerHTML = content;
-    return div;
-};
+    const div = document.createElement("div")
+
+    div.classList.add("message--self")
+    div.innerHTML = content
+
+    return div
+}
 
 const createMessageOtherElement = (content, sender, senderColor) => {
-    const div = document.createElement("div");
-    const span = document.createElement("span");
-    div.classList.add("message--other");
-    span.classList.add("message--sender");
-    span.style.color = senderColor;
-    div.appendChild(span);
-    span.innerHTML = sender;
-    div.innerHTML += content;
-    return div;
-};
+    const div = document.createElement("div")
+    const span = document.createElement("span")
+
+    div.classList.add("message--other")
+
+    span.classList.add("message--sender")
+    span.style.color = senderColor
+
+    div.appendChild(span)
+
+    span.innerHTML = sender
+    div.innerHTML += content
+
+    return div
+}
 
 const getRandomColor = () => {
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    return colors[randomIndex];
-};
+    const randomIndex = Math.floor(Math.random() * colors.length)
+    return colors[randomIndex]
+}
 
 const scrollScreen = () => {
     window.scrollTo({
         top: document.body.scrollHeight,
         behavior: "smooth"
-    });
-};
+    })
+}
 
 const processMessage = ({ data }) => {
-    const { userId, userName, userColor, content } = JSON.parse(data);
-    const message = (userId == user.id) ? createMessageSelfElement(content) : createMessageOtherElement(content, userName, userColor);
-    chatMessages.appendChild(message);
-    scrollScreen();
-};
+    const { userId, userName, userColor, content } = JSON.parse(data)
 
-const checkConnection = () => {
-    if (websocket.readyState === WebSocket.OPEN) {
-        // Envia um ping ao servidor para manter a conexão ativa
-        websocket.send("ping");
-    }
-};
+    const message =
+        userId == user.id
+            ? createMessageSelfElement(content)
+            : createMessageOtherElement(content, userName, userColor)
 
-// Estabelece a conexão WebSocket
-const establishWebSocketConnection = () => {
-    websocket = new WebSocket("wss://web-chat-back-ende.onrender.com");
+    chatMessages.appendChild(message)
 
-    websocket.onmessage = (event) => {
-        const message = event.data;
-        if (message === "pong") {
-            // Responde ao ping do servidor
-            return;
-        }
-        processMessage(event);
-    };
+    scrollScreen()
+}
 
-    websocket.onopen = () => {
-        // Define um intervalo para enviar pings periodicamente
-        setInterval(checkConnection, 30000); // Envia pings a cada 30 segundos
-    };
+const handleLogin = (event) => {
+    event.preventDefault()
 
-    websocket.onerror = (error) => {
-        console.error("Erro na conexão WebSocket:", error);
-    };
-};
+    user.id = crypto.randomUUID()
+    user.name = loginInput.value
+    user.color = getRandomColor()
 
-// Manipuladores de eventos para login e envio de mensagens
-loginForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    user.id = crypto.randomUUID();
-    user.name = loginInput.value;
-    user.color = getRandomColor();
-    loginForm.style.display = "none";
-    chatForm.style.display = "block";
-    establishWebSocketConnection();
-});
+    login.style.display = "none"
+    chat.style.display = "flex"
 
-chatForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+    websocket = new WebSocket("wss://web-chat-back-ende.onrender.com")
+    websocket.onmessage = processMessage
+}
+
+const sendMessage = (event) => {
+    event.preventDefault()
+
     const message = {
         userId: user.id,
         userName: user.name,
         userColor: user.color,
         content: chatInput.value
-    };
-    websocket.send(JSON.stringify(message));
-    chatInput.value = "";
-});
+    }
+
+    websocket.send(JSON.stringify(message))
+
+    chatInput.value = ""
+}
+
+loginForm.addEventListener("submit", handleLogin)
+chatForm.addEventListener("submit", sendMessage)
